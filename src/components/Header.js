098779b9a593964483aa75e362bf0578e0c58092
Corum Gold <goldcorum@gmail.com>
2022-08-24 18:42:42 -0400

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hello World! I'm Cory!</h1>
+      <p>
+        I'm a full stack developer who enjoys making things and working
+        alongside good people. 
+      </p>
+    </>
+  );
+};
+
+export default Header;
