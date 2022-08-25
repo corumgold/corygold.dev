e284135cd9297062cb72e0d0e9df4a4cd83a5774
Corum Gold <goldcorum@gmail.com>
2022-08-24 23:16:24 -0400

    Add about me section to home

--- src/components/Home/AboutMe.js
+++ src/components/Home/AboutMe.js
@@ -9,15 +9,21 @@ const AboutMe = () => {
       <p>
         Hi! Thanks for stopping by. I started becoming interested in web
         development when cobbled together my first website using WordPress back
-        in 2015. Since then, I have spent countless hours learning everything I
-        can about front end to back end development.
+        in 2015. I knew a little bit of HTML and CSS, how much more could there
+        be?
       </p>
       <p>
-        I have self taught and been formally trained in several different
+        Since then, I have spent countless hours learning everything I can about
+        front end to back end development. What I love so much about development
+        is the process - whether it be figuring out how to pass test specs or 
+        creating an application from the ground up, there's always something more to learn.
+      </p>
+      <p>
+        I have taught myself and been formally trained in several different
         technologies including:
       </p>
       <ul>
-        <li>JavaScript (ES6)</li>
+        <li>JavaScript (ES6+)</li>
         <li>React</li>
         <li>Node.js</li>
         <li>Express.js</li>
