128048ce7122fadfeb07afd536806de0341bea48
Corum Gold <goldcorum@gmail.com>
2022-09-07 20:52:54 -0400

    Add typewriter effect to main page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -55,20 +55,13 @@ const Home = () => {
       <div className="home-header">
         <div className="intro">
           <h1>hello world, my name is</h1>
-          <h2 className="big-header">Cory Gold.</h2>
+          <h2 className="big-header">Cory Gold</h2>
           <h3 className="big-header">I build software.</h3>
           <p>
             I'm a <span>full stack developer</span> who enjoys{" "}
             <span>making things</span> and working alongside{" "}
             <span>good people</span> on the web.
           </p>
-        </div>
-        <div>
-          <img
-            id="bunny-photo"
-            src={process.env.PUBLIC_URL + "/images/bunnypic.jpg"}
-            alt="Cory with Bunny"
-          />
         </div>
       </div>
       <AboutMe />
