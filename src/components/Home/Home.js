8287a60e6d3d08ef7d5e83c101e4701904c5e42f
Corum Gold <goldcorum@gmail.com>
2022-09-09 19:41:16 -0400

    Add better typewriter effect

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,19 +2,40 @@ import React from "react";
 import Projects from "./Projects";
 import AboutMe from "./AboutMe";
 import Footer from "./Footer";
+import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
   return (
     <>
       <div className="home-header">
         <div className="intro">
-          <h1>hello world, my name is</h1>
-          <h2 className="big-header">Cory Gold</h2>
+          <h1>hello world, I'm</h1>
+          <h2 className="big-header">
+            <Typewriter
+              className="big-header"
+              words={[
+                "Cory Gold",
+                "an Engineer",
+                "Cory Gold",
+                "a Minimalist",
+                "Cory Gold",
+                "an Artist",
+                "Cory Gold",
+                "a Designer",
+              ]}
+              loop={false}
+              cursor
+              cursorStyle="_"
+              typeSpeed={80}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </h2>
           <h3 className="big-header">I build software.</h3>
           <p>
-            I'm a <span>full stack developer</span> who enjoys{" "}
-            <span>making things</span> and working alongside{" "}
-            <span>good people</span> on the web.
+            I'm a <span>full-stack</span> engineer who enjoys making{" "}
+            <span>intuitive applications</span> and working alongside{" "}
+            <span>talented people</span> on the web.
           </p>
         </div>
       </div>
