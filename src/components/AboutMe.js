08d245dad35c45c115f6677b76020a338bde5b22
Corum Gold <goldcorum@gmail.com>
2022-09-09 22:39:42 -0400

    Change about me entries

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,7 +5,10 @@ const AboutMe = () => {
     <div id="about-me" className="about-me">
       <h4>/about_me</h4>
       <div className="profile-picture">
-        <img src={process.env.PUBLIC_URL + "/images/bunnypic.jpg"} alt="profile"/>
+        <img
+          src={process.env.PUBLIC_URL + "/images/bunnypic.jpg"}
+          alt="profile"
+        />
       </div>
       <div id="about-me-code">
         <header id="about-me-header">
@@ -14,51 +17,65 @@ const AboutMe = () => {
           <div className="circle"></div>
         </header>
         <div className="about-me-entry">
-          <p className="about-me-text">
+          <p className="about-me-query">
             {"> console.log("}
             {<span id="console-log">Cory.homeBase</span>}
             {")"}
           </p>
-          <p className="about-me-text">{'"Chattanooga, TN"'}</p>
+          <p className="about-me-response">{'"Chattanooga, TN"'}</p>
         </div>
         <div className="about-me-entry">
-          <p className="about-me-text">{"> console.log("}
+          <p className="about-me-query">
+            {"> console.log("}
             {<span id="console-log">Cory.interests</span>}
-            {")"}</p>
-          <p className="about-me-text">
+            {")"}
+          </p>
+          <p className="about-me-response">
             {
               '["minimalist design", "rock climbing", "cooking", "horror novels", "car camping"]'
             }
           </p>
         </div>
         <div className="about-me-entry">
-          <p className="about-me-text">{"> console.log("}
+          <p className="about-me-query">
+            {"> console.log("}
             {<span id="console-log">Cory.education</span>}
-            {")"}</p>
-          <p className="about-me-text">
+            {")"}
+          </p>
+          <p className="about-me-response">
             {'"B.A. Liberal Studies - PennWest California"'}
           </p>
         </div>
         <div className="about-me-entry">
-          <p className="about-me-text">{"> console.log("}
+          <p className="about-me-query">
+            {"> console.dir("}
             {<span id="console-log">Cory.skills</span>}
-            {")"}</p>
-          <p className="about-me-text">
-            {
-              '["JavaScript", "LESS", "React", "Redux", "Node", "Express", "PostgreSQL", "git"]'
-            }
+            {")"}
           </p>
+          <p className="about-me-response">frontEnd: Array(4)</p>
+          <ul>
+            <li className="array-item">0: {<span>"JavaScript"</span>}</li>
+            <li className="array-item">1: {<span>"React.js"</span>}</li>
+            <li className="array-item">2: {<span>"Redux"</span>}</li>
+            <li className="array-item">3: {<span>"LESS"</span>}</li>
+          </ul>
+          <p className="about-me-response">backEnd: Array(3)</p>
+          <ul>
+            <li className="array-item">0: {<span>"Node.js"</span>}</li>
+            <li className="array-item">1: {<span>"Express.js"</span>}</li>
+            <li className="array-item">2: {<span>"PostgreSQL"</span>}</li>
+          </ul>
         </div>
         <div className="about-me-entry">
-          <p className="about-me-text">{"> console.log("}
+          <p className="about-me-query">
+            {"> console.log("}
             {<span id="console-log">Cory.nextAdventure</span>}
-            {")"}</p>
-            <p className="about-me-text">
-            {'undefined'}
+            {")"}
           </p>
+          <p className="about-me-response">{"undefined"}</p>
         </div>
         <div className="about-me-entry">
-          <p className="about-me-text" id="about-me-cursor">{"> "}</p>
+          <p className="about-me-query">{"> "}</p>
         </div>
       </div>
     </div>
