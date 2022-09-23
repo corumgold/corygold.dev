a3264a7995511a1a8e46b260701a37c4060e2f08
Corum Gold <goldcorum@gmail.com>
2022-09-23 10:39:36 -0400

    Fix typo

--- src/projects.js
+++ src/projects.js
@@ -5,7 +5,7 @@ const projects = [
     image: process.env.PUBLIC_URL + `/images/simpli_money.png`,
     altText: "simpli money project image",
     description:
-      "My idea behind this app is to help people get ahold of their finances by providing research-backed (and cited) advice for folks who are unsure how to go about saving for emergencies, paying off debt, saving for retirment, etc.",
+      "My idea behind this app is to help people get ahold of their finances by providing research-backed (and cited) advice for folks who are unsure how to go about saving for emergencies, paying off debt, saving for retirement, etc.",
     projectLink: "https://github.com/corumgold/simpli-money",
     liveLink: "https://www.simpli.money",
     tools: ["JavaScript", "LESS", "React", "Node", "Redux"],
